87502c9c0e40eac57c997d10e920b1559dea41be
Tama <mytamhuynhhmt@gmail.com>
2025-02-21 22:40:09 +0700

    Bài tập Login, Sign in

---
+++ app/src/main/java/com/example/myapplication2/BTLogin.java
@@ -0,0 +1,31 @@
+package com.example.myapplication2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BTLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ConstraintLayout bg = (ConstraintLayout)
+                findViewById(R.id.main);
+        bg.setBackgroundColor(Color.BLUE);
+        bg.setBackgroundResource(R.drawable.top_background2);
+    }
+}
