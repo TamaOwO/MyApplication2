2d225cef44d854e0cdb11ae65e49baf1807a6abf
Tama <mytamhuynhhmt@gmail.com>
2025-02-21 10:40:06 +0700

    Bai 2 Switch

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication2;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,11 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch switchBackgournd;
+    int cur;
+    int last;
+    ArrayList<Integer> arrayList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +31,34 @@ public class MainActivity extends AppCompatActivity {
         bg.setBackgroundColor(Color.BLUE);
         bg.setBackgroundResource(R.drawable.background_intro);
 
-        //Random background
-        ArrayList<Integer> arrayList = new ArrayList<>();
+
         arrayList.add(R.drawable.logo);
         arrayList.add(R.drawable.pizza);
         arrayList.add(R.drawable.background_intro);
         arrayList.add(R.drawable.pop_3);
+
+        //Random background
         Random random = new Random();
         int vitri = random.nextInt(arrayList.size());
         bg.setBackgroundResource(arrayList.get(vitri));
+        cur = vitri;
+
+        chuyenBackground();
+    }
+
+    private void chuyenBackground(){
+        ConstraintLayout bg = (ConstraintLayout) findViewById(R.id.constraintLayout1);
+        Switch switchBackgournd = (Switch) findViewById(R.id.switch1);
+        switchBackgournd.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                last = cur;
+                cur = (cur + 1) % arrayList.size();
+                bg.setBackgroundResource(arrayList.get(cur));
+            } else {
+                cur = last;
+                bg.setBackgroundResource(arrayList.get(last));
+            }
+
+        });
     }
 }
